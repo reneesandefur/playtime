126ade340293af2faa17fa911df48fc249462b7a
Renee Sandefur <reneersandefur@gmail.com>
2017-10-23 03:21:26 +0000

    added friendly id gem, updated controller for friendly, updated model with extend friendly id and paramats for updating slug

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -17,4 +17,11 @@ class Wishlist < ApplicationRecord
 
   validates :name, presence: true,
                    uniqueness: true
+                   
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :history]
+  
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
 end
