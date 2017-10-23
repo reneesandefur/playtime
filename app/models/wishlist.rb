7f24879d69f08b1d0ed0ccc571664bcf561ce6b1
Renee Sandefur <reneersandefur@gmail.com>
2017-10-23 02:40:46 +0000

    added params for name change to wishlist model

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -19,5 +19,10 @@ class Wishlist < ApplicationRecord
                    uniqueness: true
                    
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :name, use: [:slugged, :history]
+  
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
+end
 end
